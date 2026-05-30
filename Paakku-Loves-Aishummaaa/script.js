fd0f355670729e05a7271998645a47fb6d03dd6f
ajithbhaskar3585 <ajithbhaskar3585@gmail.com>
2026-05-30 12:06:45 +0530

    Add files via upload

---
+++ Paakku-Loves-Aishummaaa/script.js
@@ -0,0 +1,25 @@
+function showLetter() {
+
+document.getElementById("message").innerHTML =
+
+`
+<h2>My Dear Aishummaaa ❤️</h2>
+
+<p>
+
+Thank you for being part of my life for 4½ wonderful years.
+
+You still make my heart smile every day.
+
+I don't know what tomorrow holds,
+
+but I know I want you in it.
+
+Forever yours,
+
+Paakku ❤️
+
+</p>
+`;
+
+}
